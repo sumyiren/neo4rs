0eaadbadcc21fc460d398652e1e07f5f8f0e2fa4
sumyiren <sumyiren@gmail.com>
2022-07-08 16:39:21 +0100

    commented out

--- lib/src/internal/transaction_executor.rs
+++ lib/src/internal/transaction_executor.rs
@@ -32,15 +32,15 @@ impl TransactionExecutor {
 
     pub async fn run_transaction<F> (&self, mut txn: Txn, access_mode: AccessMode, transaction_work: F) where F: Fn(&'_ mut Txn) -> BoxFuture<'_, Result<()>> {
         let res = transaction_work(&mut txn).await;
-        match res {
-            Err(E) => {
-                if let Error::UnexpectedMessage(e) = E {
-                    println!("{}", e);
-                    self.retry_transaction(txn, transaction_work, -1, self.initial_retry_delay_ms as i64).await;
-                }
-            }
-            Ok(_) => { txn.commit().await; }
-        }
+        // match res {
+        //     Err(E) => {
+        //         if let Error::UnexpectedMessage(e) = E {
+        //             println!("{}", e);
+        //             self.retry_transaction(txn, transaction_work, -1, self.initial_retry_delay_ms as i64).await;
+        //         }
+        //     }
+        //     Ok(_) => { txn.commit().await; }
+        // }
     }
 
     pub fn close() {
@@ -48,22 +48,22 @@ impl TransactionExecutor {
 
     }
 
-    async fn retry_transaction<F>(&self, mut txn: Txn, transaction_work: F, mut start_time: i64, retry_delay_ms: i64) where F: Fn(&'_ mut Txn) -> BoxFuture<'_, Result<()>> {
-        let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as i64;
-        if start_time == -1 {
-            start_time = current_time;
-        }
-
-        let elapsed_time = current_time - start_time;
-        let delay_with_jitter_ms = self.compute_delay_with_jitter(retry_delay_ms);
-        let new_retry_delay_ms = retry_delay_ms as f32 * self.multiplier;
-        if elapsed_time < self.max_retry_time_ms as i64 {
-            sleep(Duration::from_millis(delay_with_jitter_ms as u64)).await;
-            async move {
-                self.retry_transaction(txn, transaction_work, start_time, new_retry_delay_ms as i64).await;
-            }.boxed()
-        }
-    }
+    // async fn retry_transaction<F>(&self, mut txn: Txn, transaction_work: F, mut start_time: i64, retry_delay_ms: i64) where F: Fn(&'_ mut Txn) -> BoxFuture<'_, Result<()>> {
+    //     let current_time = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() as i64;
+    //     if start_time == -1 {
+    //         start_time = current_time;
+    //     }
+    //
+    //     let elapsed_time = current_time - start_time;
+    //     let delay_with_jitter_ms = self.compute_delay_with_jitter(retry_delay_ms);
+    //     let new_retry_delay_ms = retry_delay_ms as f32 * self.multiplier;
+    //     if elapsed_time < self.max_retry_time_ms as i64 {
+    //         sleep(Duration::from_millis(delay_with_jitter_ms as u64)).await;
+    //         async move {
+    //             self.retry_transaction(txn, transaction_work, start_time, new_retry_delay_ms as i64).await;
+    //         }.boxed()
+    //     }
+    // }
 
     fn compute_delay_with_jitter(&self, mut delay_ms: i64) -> f32 {
         if delay_ms > MAX_RETRY_DELAY {
@@ -72,7 +72,7 @@ impl TransactionExecutor {
 
         let jitter = delay_ms as f32 * self.jitter_factor;
         let min = delay_ms as f32 - jitter;
-        let max = delay_ms as few + jitter;
+        let max = delay_ms as f32 + jitter;
         return rand::thread_rng().gen_range(min..max); // todo - check thread local one like the java impl
     }
 }
